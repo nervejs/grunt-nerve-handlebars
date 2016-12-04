7c02ac16f43015cdcb622b1b541f3b6d4ccede38
Yarkeev Denis <d.yarkeev@corp.mail.ru>
2016-12-04 03:06:25 +0300

    iteration

--- tasks/nerve-handlebars.js
+++ tasks/nerve-handlebars.js
@@ -1,4 +1,4 @@
-'use strict';
+/*jslint regexp: true */
 
 module.exports = function (grunt) {
     'use strict';
@@ -7,7 +7,7 @@ module.exports = function (grunt) {
         Handlebars = require('handlebars'),
         n2a = require('native2ascii').native2ascii;
 
-    grunt.registerMultiTask("nerve-handlebars", "translate handlebars templates to js", function () {
+    grunt.registerMultiTask('templates', 'translate templates to handlebars', function () {
         var options = this.options();
 
         function buildPartials(source, fileName) {
@@ -16,46 +16,67 @@ module.exports = function (grunt) {
 
             if (includes) {
                 includes.forEach(function (item) {
-                    var relativeFileName = item.replace(/\{|\}|>/g, '') + '.hbs';
+                    var relativeFileName = item.replace(/\{|\}|>/g, ''),
+                        fullIncludeFileName,
+                        ext = path.extname(relativeFileName),
+                        isPathPrefixFound = false;
 
-                    fileName = path.resolve(dir, relativeFileName);
-                    source = source.replace(item, buildPartials(grunt.file.read(fileName), fileName));
+                    if (ext !== '.html') {
+                        relativeFileName += '.hbs';
+                    }
+
+                    Object.keys(options.paths).forEach(function (pathId) {
+                        if (relativeFileName.indexOf('@' + pathId) !== -1) {
+                            fullIncludeFileName = path.resolve(options.paths[pathId], relativeFileName.replace('@' + pathId + '/', ''));
+                            isPathPrefixFound = true;
+                        }
+                    });
+
+                    if (!isPathPrefixFound) {
+                        fullIncludeFileName = path.resolve(dir, relativeFileName);
+                    }
+
+                    if (!grunt.file.exists(fullIncludeFileName)) {
+                        grunt.fail.fatal('no such file or directory: ' + fullIncludeFileName + '\nin file ' + fileName);
+                    } else {
+                        source = source.replace(item, buildPartials(grunt.file.read(fullIncludeFileName), fullIncludeFileName));
+                    }
                 });
             }
+
             return source;
         }
 
         this.filesSrc.forEach(function (file) {
-            var cwd = this.data.cwd || '',
-                dst = this.data.dst || '',
-                filePath = path.resolve(cwd, file),
+            var filePath = path.resolve(this.data.cwd, file),
                 moduleName = file.split('/')[0],
                 relativeFileName = file.replace(moduleName + '/tmpl/', ''),
-                dstPath = path.resolve(path.resolve(dst, moduleName, 'tmpl', relativeFileName.replace(/(\.html)|(\.hbs)/, '.js'))),
+                dstPath = path.resolve(options.dst, moduleName, options.tmplDir || '', relativeFileName.replace(/(\.html)|(\.hbs)/, '.js')),
                 tmplSource = grunt.file.read(filePath, {encoding: 'utf-8'}),
-                handleBarsJs;
-
-            if (this.data.isSimplePath) {
-                dstPath = path.resolve(dst, file.replace(/\.hbs$/, '.js'));
-            }
+                handleBarsJs,
+                precompile;
 
             tmplSource = buildPartials(tmplSource, filePath);
+            try {
+                precompile = Handlebars.precompile(tmplSource);
+            } catch (err) {
+                console.log(err);
+                grunt.fail.fatal('cannot compile ' + file);
+            }
 
             if (options.isCommonJs) {
-                handleBarsJs = " " +
-                    "Handlebars = require('handlebars');" +
-                    "helpers = require('node-nerve').HandlebarsHelpers;" +
-                    "helpers(Handlebars);" +
-                    "module.exports = Handlebars.template(" + Handlebars.precompile(tmplSource) + ");";
+                handleBarsJs = ' ' +
+                    'Handlebars = require(process.cwd() + \'/src/app\').Handlebars;' +
+                    'module.exports = Handlebars.template(' + precompile + ');';
             } else {
-                handleBarsJs = " " +
-                    "define([" +
-                    "   'handlebars'," +
-                    "   '" + options.helpersPath + "'" +
-                    "], function (Handlebars, helpers) {" +
-                    "       helpers(Handlebars);" +
-                    "       return Handlebars.template(" + Handlebars.precompile(tmplSource) + ");" +
-                    "});";
+                handleBarsJs = ' ' +
+                    'define([' +
+                    '   \'handlebars\',' +
+                    '   \'' + options.helpersPath + '\'' +
+                    '], function (Handlebars, helpers) {' +
+                    '       helpers(Handlebars);' +
+                    '       return Handlebars.template(' + precompile + ');' +
+                    '});';
             }
 
             grunt.file.mkdir(path.dirname(dstPath));
